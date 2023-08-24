db9eefdfd97495d7dde75ca07eb048e380564ac5
Jason Kozel <jason.kozel@crowdbotics.com>
2023-08-24 06:34:31 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "ent-cyber-insurance-43045",
-  url: "https://ent-cyber-insurance-43045.botics.co",
-  api: "https://ent-cyber-insurance-43045.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "ent-cyber-insurance-43045",
+  "url": "https://ent-cyber-insurance-43045.botics.co",
+  "api": "https://ent-cyber-insurance-43045.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
